5de337f891f95e4b9c88dba08f33ddc768ff4b57
suniannb <81002572+suniannb@users.noreply.github.com>
2021-07-22 20:51:13 +0800

    Create 连接数.sh

---
+++ 连接数.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#获取80端口链接ip
+netstat -na|grep 80|awk '{print $5}'|awk -F: '{print $1}' >A;
+#获取本机ip
+IP=`curl -s ip.cip.cc`;
+#去重
+sort A | sort -u A > B ;
+#查询前过滤
+sed -i "s/STREAM//g" B>A;
+sed -i "s/$IP//g" A>B;
+#计数变量
+JS=`sed -n '$=' B`;
+echo "当前连 接ip数为$((JS-2)) 个";
+#逐行读取并进行ip溯源
+i=1;
+while read myline;
+do
+        echo -e "IP溯源信息$i:\n `curl -Ls http://www.cip.cc/$myline| egrep -v "URL" | sed '/^[  ]*$/d'`";
+        let i=$i+1;
+        sleep 1;
+done < B;
