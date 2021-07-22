665caae684c889396bd7893c2b426aaaa4f79488
suniannb <81002572+suniannb@users.noreply.github.com>
2021-07-22 21:11:43 +0800

    Update 连接数.sh

--- 连接数.sh
+++ 连接数.sh
@@ -4,9 +4,10 @@ netstat -na|grep 80|awk '{print $5}'|awk -F: '{print $1}' >A;
 #去重
 sort A | sort -u A > B ;
 #查询前过滤
-sed -i "s/STREAM//g" B;
+sed -i "s/STREAM//g" B ;
+sed -i "s/`curl -s ip.cip.cc`//g" B;
 #计数变量
-JS=`sed -n '$=' B`;
+JS=`sed -n '$=' A`;
 echo "当前连 接ip数为$((JS-2)) 个";
 #逐行读取并进行ip溯源
 i=1;
@@ -15,4 +16,4 @@ do
         echo -e "IP溯源信息$i:\n `curl -Ls http://www.cip.cc/$myline| egrep -v "URL" | sed '/^[  ]*$/d'`";
         let i=$i+1;
         sleep 1;
-done < B;
+done < A;
